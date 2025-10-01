3165bf79c975af395bf2649c5a6d58d84374c8d8
Sunisa77 <sunisa.bn@rmuti.ac.th>
2025-10-01 11:03:11 +0700

    แยกส่วนต่างๆ

---
+++ resources/views/layouts/front/js.blade.php
@@ -0,0 +1,13 @@
+<!-- THEME JAVASCRIPT FILES
+================================================== -->
+<!-- initialize jQuery Library -->
+<script src="{{asset('asset/front/plugins/jquery/jquery.js')}}"></script>
+<!-- Bootstrap jQuery -->
+<script src="{{asset('asset/front/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+<!-- Slick Slider -->
+<script src="{{asset('asset/front/plugins/slick-carousel/slick.min.js')}}"></script>
+<!-- Google Map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC72vZw-6tGqFyRhhg5CkF2fqfILn2Tsw"></script>
+<script src="{{asset('asset/front/plugins/google-map/gmap.js')}}"></script>
+<!-- main js -->
+<script src="{{asset('asset/front/js/custom.js')}}"></script>
